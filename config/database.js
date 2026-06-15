94283fd28e432a3b1d54ceb2ae3c9567e296215f
Hoang Phuc <hoangphuc210905@gmail.com>
2026-06-15 10:01:58 +0700

    Code test

--- config/database.js
+++ config/database.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
 
+let cachedConnection = null;
+
 const connectDB = async () => {
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URL environment variable is missing");
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    cachedConnection = await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected");
+    return cachedConnection;
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
 };
 
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const database = require("./config/database");
 const bookingRoutes = require("./routes/bookingRoutes");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set("view engine", "ejs");
-app.set("views", "./views");
-database();
+app.set("views", path.join(__dirname, "views"));
+
+app.use(async (req, res, next) => {
+  try {
+    await database();
+    next();
+  } catch (error) {
+    res.status(500).send(`<h1>Database Connection Error</h1><p>${error.message}</p>`);
+  }
+});
+
 app.use("/bookings", bookingRoutes);
 app.get("/", (req, res) => {
   res.redirect("/bookings");
 });
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running at http://localhost:${process.env.PORT}`);
+  console.log(`Server running at http://localhost:${process.env.PORT || 3000}`);
 });
 module.exports = app;
